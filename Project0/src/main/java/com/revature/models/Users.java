c57c800df4387f73b5772461c68fa1486a793bae
Tyler Throop <tyler.throop@gmail.com>
2021-05-26 14:44:05 -0700

    Adding a bit more functionality

--- Project0/src/main/java/com/revature/models/Users.java
+++ Project0/src/main/java/com/revature/models/Users.java
@@ -1,7 +1,11 @@
 package com.revature.models;
 
-public abstract class Users {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Users implements Account{
 		String userName, password;
+		int Balance;
+		List<Account> userAccounts = new ArrayList<Account>();
 		
-	
 }
