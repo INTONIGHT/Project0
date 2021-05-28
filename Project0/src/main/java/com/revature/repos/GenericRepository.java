c51989b09a3b6d877db3221c261faca25f222172
Tyler Throop <tyler.throop@gmail.com>
2021-05-27 19:21:59 -0700

    Adding the generic methods to the generic repo will implement in more repos further down the road

--- Project0/src/main/java/com/revature/repos/GenericRepository.java
+++ Project0/src/main/java/com/revature/repos/GenericRepository.java
@@ -1,5 +1,14 @@
 package com.revature.repos;
 
-public interface GenericRepository {
+import java.util.List;
 
+public interface GenericRepository <T>{
+	//implementing CRUD operations to create
+	//read update and destroy.
+	public T add(T t);
+	public T getById(Integer id);
+	public List<T> getAll();
+	public void update(T change);
+	public void delete(T t);
+	
 }
