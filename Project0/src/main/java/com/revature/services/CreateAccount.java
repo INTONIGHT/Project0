28d34f320fc37e9e567e0d1ebb5a0508b9af6034
Tyler Throop <tyler.throop@gmail.com>
2021-06-01 13:29:18 -0700

    Adding a check so that a user can type a number to chose the option. this will make it nicer to add functions and checks within the switch statements.

--- Project0/src/main/java/com/revature/services/CreateAccount.java
+++ Project0/src/main/java/com/revature/services/CreateAccount.java
@@ -2,4 +2,8 @@ package com.revature.services;
 
 public class CreateAccount {
 
+	public void createAccount() {
+		
+	}
+	
 }
--- Project0/src/main/java/com/revature/services/ProcessInput.java
+++ Project0/src/main/java/com/revature/services/ProcessInput.java
@@ -6,7 +6,7 @@ import com.revature.models.User;
 import com.revature.repos.EmployeeRepository;
 import com.revature.repos.UserRepository;
 import com.revature.utils.MockDB;
-
+import com.revature.services.CreateAccount;
 public class ProcessInput {
 
 	public void processString() {
@@ -15,10 +15,11 @@ public class ProcessInput {
 		//for now this is dummy data later on we will change this
 		UserRepository ur = new UserRepository();
 		EmployeeRepository er = new EmployeeRepository();
+		CreateAccount ca = new CreateAccount();
 		boolean running = true;
 		String username, password ;
 		User realUser; 
-		int findUser ;
+		int findUser ,switchCase ;
 		//might change this
 		boolean findPass;
 		//ints to find the username and password in the list maybe.
@@ -26,41 +27,58 @@ public class ProcessInput {
 		//ok it doesnt let you log out yet.
 		
 		while(running) {
-			System.out.println("Type Logout to leave the program");
-			System.out.println("Please type in your user name:");
-			username = in.nextLine();
-			System.out.println("Please type in your password:");
-			password = in.nextLine();
-			
-			findUser = ur.getUsername(username);
-			//now it should return a boolean.
-			findPass = ur.getPassword(username,password);
-			
-			realUser = ur.getById(findUser);
-			
-			
-			//here we will want to verify that the username
-			//and password are correct.
-			
-			//|| username.equals(er.getUsername(username)) && password.equals(er.getPassword(password))
-		
-			System.out.println(username.equals(realUser.getUsername()) + " this is the truth value");
-			try {
-			if(username.equals(realUser.getUsername()) && findPass == true) {
-				//can now do what I want here as it works as intended.
-				System.out.println(realUser);
+			System.out.println("Type 1 to create an account 2 to login 3 to logout");
+			switchCase = in.nextInt();
+			switch(switchCase) {
+			case 1:
+				ca.createAccount();
+				break;
+			case 2 :
+				//it looks like username is set to the in.nextInt()
+				System.out.println("Please type in your user name:");
+				username = in.next();
+				System.out.println("Please type in your password:");
+				password = in.next();
 				
-			} else if(in.nextLine().equals("Logout")) {
+				findUser = ur.getUsername(username);
+				//now it should return a boolean.
+				findPass = ur.getPassword(username,password);
+				
+				realUser = ur.getById(findUser);
+				
+				try {
+				if(username.equals(realUser.getUsername()) && findPass == true) {
+					//can now do what I want here as it works as intended.
+					System.out.println(realUser);
+					//it will go back to the while not sure what to do then 
+					//maybe escape the while and or run another function
+					//so that a user can interact with their account.
+				} else {
+					System.out.println("Please retype your username and password");
+				}
+				} catch(NullPointerException e) {
+					System.out.println(ur.getUsername(username));
+					System.out.println(e.getStackTrace());
+				}
+			break;
+			case 3:
+				System.out.println("Have a good day");
 				running = false;
-			}else {
-				System.out.println("Please retype your username and password");
+				break;
+			default :
+				System.out.println("Please type a number between 1 and 3");
 			}
-			} catch(NullPointerException e) {
-				System.out.println(ur.getUsername(username));
-				System.out.println(e.getStackTrace());
-			}
+			
+			
+			
+			
+		
+			
 		}
 		
 	}
-	
+	public void login() {
+		System.out.println("Please type Deposit and then an amount to deposit an amount in you account");
+		System.out.println("\n Type withdraw to withdraw an amount");
+	}
 }
